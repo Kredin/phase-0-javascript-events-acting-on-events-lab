a6a9fecc0bbcdd96a266f7d84f0f42ea88b99fd3
Blade <bladeprojectz@gmail.com>
2024-02-12 16:14:08 +0800

    added event for up and down motion

--- index.js
+++ index.js
@@ -21,11 +21,35 @@ function moveDodgerRight() {
     }
 }
 
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom < 380) {
+      dodger.style.bottom = `${bottom + 5}px`;
+    }
+}
+
+function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom > 0) {
+      dodger.style.bottom = `${bottom - 5}px`;
+    }
+}
+
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }else if (e.key === "ArrowRight") {
         moveDodgerRight()
+    }
+    else if (e.key === "ArrowUp") {
+        moveDodgerUp()
+    }
+    else if (e.key === "ArrowDown") {
+        moveDodgerDown()
     }
 });
